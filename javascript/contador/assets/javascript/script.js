77a6445fedd6ec6de39600fd3e9d8cfb112a0f6b
Gleybson Farias <gleybson.farias@gmail.com>
2022-01-22 11:09:25 -0300

    criando pasta contador. Movendo arquivos para pasta contador. Regra para contador: O número do contador ficará vermelho caso o número seja menor que zero

--- javascript/contador/assets/javascript/script.js
+++ javascript/contador/assets/javascript/script.js
@@ -4,9 +4,15 @@ var currentNumber = 0;
 function increment(){
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    toCheckNumber()
 }
 
 function decrement(){
     currentNumber = currentNumber - 1 ;
     currentNumberWrapper.innerHTML = currentNumber;
+    toCheckNumber()
+}
+
+function toCheckNumber(){
+    currentNumber < 1 ? currentNumberWrapper.style.color = 'red': currentNumberWrapper.style.color = 'black'
 }
