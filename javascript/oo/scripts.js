dbaaf7c571bd7ac5e5447f7f3dc2bcf07617e56f
Gleybson Farias <gleybson.farias@gmail.com>
2022-01-30 23:44:33 -0300

    orientaçao a objetos

---
+++ javascript/oo/scripts.js
@@ -0,0 +1,68 @@
+class ContaBancaria{
+
+    construtor(agencia, numero, tipo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo =  0;
+    }
+
+
+    get saldo(){
+        return this._saldo;
+
+    }
+
+    set saldo(valor){
+        this._saldo = valor;
+    }
+
+    sacar(valor){
+        if(valor > this._saldo ) return 'operacao negada';
+        this._saldo = this._saldo - valor;
+        return this._saldo
+    }
+
+    depositar(valor){
+        this._saldo = this._saldo + valor;
+    }
+
+}
+
+class ContaCorrente extends ContaBancaria{
+    constructor(agencia, numero, tipo, cartaoCredito){
+        super(agencia, numero, tipo);
+        this.tipo = "corrente";
+        this._cartaoCredito = cartaoCredito;
+    }
+
+    get cartaoCredito(){
+        return this._cartaoCredito;
+    }
+
+    set cartaoCredito(valor){
+        this._cartaoCredito = valor;
+    }
+
+}
+
+class ContaPoupanca extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero);
+        this.tipo = "poupança";
+    }
+}
+
+class ContaUniversitaria extends ContaBancaria{
+    constructor(agencia, numero){
+        super(agencia, numero );
+        this._tipo = "universitária";
+    }
+
+    sacar(valor){
+        if ( valor > 500) return 'operação negada';
+        this._saldo = this._saldo - valor;
+    }
+
+
+}
